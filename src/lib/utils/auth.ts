32992448721382801280de8fd6cd6e76fd3959d3
Praveen Kumar <pkspyder007@gmail.com>
2022-09-11 14:00:12 +0530

    fix: check auth middleware executing for all routers

--- src/lib/utils/auth.ts
+++ src/lib/utils/auth.ts
@@ -4,26 +4,6 @@ import { v4 as uuid } from "uuid";
 import jwt from "jsonwebtoken";
 import config from "../config";
 
-exports.checkAuth = (req: Request, res: Response, next: NextFunction) => {
-  const authtkn = req.cookies.authtkn;
-  if (!authtkn) {
-    return res
-      .status(400)
-      .json({ msg: "No auth token found. Please login again." });
-  }
-  try {
-    let decoded = jwt.verify(authtkn, config.get("JWT_SECRET"));
-    req.user = decoded;
-    // console.log("Data: " , decoded);
-  } catch (error) {
-    return res
-      .status(400)
-      .json({ msg: "Invalid auth token. Please login again." });
-  }
-
-  next();
-};
-
 export const createEmailVerificationToken = (): string => {
   return uuid();
 };
--- src/routes/complaints.ts
+++ src/routes/complaints.ts
@@ -4,19 +4,17 @@ import { checkAuth, isAdmin } from "../middlewares/auth";
 
 const complainsRouter = Router();
 
-complainsRouter.use(checkAuth);
+complainsRouter.get('/complaints', checkAuth, getAllComplaintsForUser);
 
-complainsRouter.get('/complaints', getAllComplaintsForUser);
+complainsRouter.post('/complaints', checkAuth, createComplaint);
 
-complainsRouter.post('/complaints', createComplaint);
+complainsRouter.get('/complaints/all', checkAuth, isAdmin, getAllComplaintsForAdmin);
 
-complainsRouter.get('/complaints/all', isAdmin, getAllComplaintsForAdmin);
+complainsRouter.post('/complaints/remarks', checkAuth, addRemark);
 
-complainsRouter.post('/complaints/remarks', addRemark);
+complainsRouter.post('/complaints/status', checkAuth, updateStatus);
 
-complainsRouter.post('/complaints/status', updateStatus);
-
-complainsRouter.post('/complaints/escalate', escalate);
+complainsRouter.post('/complaints/escalate', checkAuth, escalate);
 
 
 export default complainsRouter;
